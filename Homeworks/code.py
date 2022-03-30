7ed364caaa192d73b81a02ccc9df20a1773926b4
MAMN0 <mamno653@gmail.com>
2022-03-30 14:28:25 +0400

    hallo world

---
+++ Homeworks/code.py
@@ -0,0 +1,10 @@
+# # just my practical code file, i will train in this file
+# # In First Lesson we learned function print and math algorithms
+# # We learned logic in scratch so i think everything will be normal
+# #Lesson 1
+
+# print("Hallo Nikusha, I am jarvis.")
+# print("I am 13 years old and i'l teach you PYTHON.")
+
+# #Homework 1
+# was very eazy
---
+++ Lessons/Lesson1.py
@@ -0,0 +1,16 @@
+# print(type(53))
+# print(type(5.4))
+# print(type(True))
+# print(type("Hallo"))
+
+# print(123)
+# print("I am", 13, "years old")
+# print("hallo, world")
+
+
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 2)
+print(2 ** 9)
+print(2 % 2)
