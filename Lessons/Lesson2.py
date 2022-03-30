4ce1e86550f7edc8eb492cc1d315f03df081bdff
MAMN0 <mamno653@gmail.com>
2022-03-30 15:24:38 +0400

    Lesson 2

---
+++ Lessons/Lesson2.py
@@ -0,0 +1,54 @@
+# name = "nikusha"
+# last_name = "Mamniashvili"
+# print(name)
+# print("hallo", name)
+# print("Hallo", name, last_name)
+
+# a = 3
+# b = 4
+# c = 5
+#
+# a, b, c = "nikusha", 5, True
+#
+# print(a, c, b)
+
+# x = 5
+# x = 3
+# x += 1
+# ეს არის ცვლადის მომატება ერთით
+# print(x)
+# ცვლადს არ შეიძლება დაერქვას ისეთი სახელი, რომელსაც პირველი სიმბოლო რიცხვი აქვს
+#
+# age = 13
+# print(name, "is", age, "years old")
+
+# a, b = 3, 4
+# c = a * b
+# print(c)
+
+# +, -, *, /, **, //, %
+#
+# a, b = 5, 3
+# print("a / b =", a / b)
+# print("a // b =", a // b)
+# print("a + b =", a + b)
+# print("a - b =", a - b)
+# print("a * b =", a * b)
+# print("a ** b =", a ** b)
+
+# x = 4
+# x += 1
+# # +=, -=, *=, /=, **=, //=, %=
+# print(x)
+
+# x = -3
+# print(abs(x))
+# print(pow(x, 3)) # operation functions
+#
+# y = 5.42
+#
+# print(round(y, 2))
+# print(int(y))
+
+# x = int(input("Input Any Number "))
+# print(x)
