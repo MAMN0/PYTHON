241553fbb8ec8b1207b709578482dfa1bd4e5ddd
MAMN0 <mamno653@gmail.com>
2022-03-30 19:40:35 +0400

    Homework2

---
+++ Homeworks/Homework2.py
@@ -0,0 +1,64 @@
+Example = 1
+print()
+print(Example, "Example")
+print()
+message = "What’s up, Tim?"
+print(message)
+Example += 1
+print()
+# 1 completed
+print(Example, "Example")
+print()
+a, b = 1, 4
+print(a, b)
+a, b = b, a
+print(a, b)
+print()
+Example += 1
+# 2 completed
+print(Example, "Example")
+print()
+name, last_name = input("Input Your Name: "), input("Input Your Last Name: ")
+print()
+print("Hello", name, last_name)
+print()
+Example += 1
+# 3 completed
+print()
+print(Example, "Example")
+print()
+
+work_time = input("შეიყვანე შენი სამუშაო საათები თვეში: ")
+work_money = input("შეიყვანე შენი საათობრივი სახელფასო განაკვეთი: ")
+print()
+money = int(work_time) * int(work_money)
+print("შენი შემოსავალია", money)
+print()
+Example += 1
+# 4 complected
+print(Example, "Example")
+print()
+n1, n2, n3= input("Input Number: "), input("Input Number: "), input("Input Number: ")
+x = (int(n1)+int(n2)+int(n3))/3
+Example += 1
+
+print()
+print(x)
+# 5 completed
+print()
+print(Example, "Example")
+print()
+name = input("Input Name: ")
+age = input("Input Age: ")
+years100 = 2022 + (100 - int(age))
+
+print()
+print(name, "You Will Be 100 Years Old in", years100)
+print()
+Example += 1
+# 6 complected
+print(Example, "Example")
+print()
+C = input("Input Temperature: ")
+F = (int(C) * 9 / 5) + 32
+print("Your Temperature in Farenhait is", F)
