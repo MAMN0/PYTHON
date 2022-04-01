523ed4a766a837a60ccbd7032eb911b8d33ec420
MAMN0 <mamno653@gmail.com>
2022-04-01 17:06:48 +0400

    Homework3

---
+++ Lessons/Lesson3.py
@@ -0,0 +1,55 @@
+# Lesson 3
+# True, False
+
+# print(type(True))
+# print(type(False))
+
+# print(True)
+# print(not True)
+# print(not not True)
+# print(not not not True)
+
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# x, y = 1, 1
+#
+# if x > y:
+#     print("+")
+# else:
+#     print("-")
+
+# x, y = 1, 1
+#
+# if x == y:
+#     print("+")
+# else:
+#     print("-")
+#
+# x, y = 1, 1
+#
+# if x == y:
+#     print("+")
+# else:
+#     print("-")
+
+# x, y = 1, 1
+#
+# if x != y:
+#     print("+")
+# else:
+#     print("-")
+
+# x, y = 1, 1
+#
+# if x <= y or x > y:
+#     print("+")
+# else:
+#     print("-")
