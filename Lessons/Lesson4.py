875a9facf883392b7c9f4103db29261904f78a0d
MAMN0 <mamno653@gmail.com>
2022-04-06 15:24:43 +0400

    Lesson4

--- Lessons/Lesson4.py
+++ Lessons/Lesson4.py
@@ -1,18 +1,15 @@
-Number = int(input("Input Number 0-100"))
-if Number > 0 and Number < 100:
-elif Number > 91 and Number < 100:
+Number = int(input("Input Number 0-100: "))
+if Number >= 91 and Number <= 100:
     print("A")
-elif Number > 81 and Number < 90:
+elif Number >= 81 and Number <= 90:
     print("B")
-elif Number > 71 and Number < 80:
+elif Number >= 71 and Number <= 80:
     print("C")
-elif Number > 61 and Number < 70:
+elif Number >= 61 and Number <= 70:
     print("D")
-elif Number > 51 and Number < 60:
+elif Number >= 51 and Number <= 60:
     print("E")
-elif Number > 41 and Number < 50:
+elif Number >= 41 and Number <= 50:
     print("FX")
-elif Number > 0 and Number < 40:
+elif Number >= 0 and Number <= 40:
     print("Failed")
-else:
-    print()
