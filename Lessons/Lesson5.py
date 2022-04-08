632eeb44a6d68c7fcad9c5e87d9931251d25ef0e
MAMN0 <mamno653@gmail.com>
2022-04-08 16:56:33 +0400

    Lesson5

---
+++ Lessons/Lesson5.py
@@ -0,0 +1,65 @@
+# x = 1
+# Loops
+# while 3 > 2:
+#     print('gamarjoba')
+# while True:
+#     print("N")
+#     break
+# print("hallo")
+# while True:
+#     print("gamarjoba")
+#     print("gamarjoba")
+#     continue
+#     print("gamarjoba")
+
+# print("hallo")
+# x = 1
+# while True:
+#     print(x)
+#     x += 2
+
+# x = 1
+# while True:
+#     x += 1
+#     if x % 4 == 0:
+#         print(x)
+#     if x > 10:
+#         break
+
+# x = 1
+# while True:
+#     x += 1
+#     if x % 4 != 0:
+#         continue
+#     print(x)
+#     if x > 10:
+#         break
+
+# while x < 11:
+#     print(x)
+#     x += 1
+
+# for i in range(2):
+#     print(i + 1)
+
+# for giorgi in range(10, 20):
+#     print(giorgi)
+
+# for i in range(10, 21, 5):
+#     print(i)
+
+# for i in range(1, 10):
+#     if i % 2 == 0:
+#          print(i)
+# for i in range(20):
+#     print(i)
+#     break
+
+# start = 2
+# calendar = 18
+# step = 3
+#
+# for i in range(start, calendar, step):
+#     print(i)
+#     if i > 5:
+#         break
