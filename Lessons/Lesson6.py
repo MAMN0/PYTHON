e3f7732858d2bc122a07c8da8334a08e3ad7d9f4
MAMN0 <mamno653@gmail.com>
2022-04-13 23:12:23 +0400

    Lesson6

--- Lessons/Lesson6.py
+++ Lessons/Lesson6.py
@@ -5,13 +5,14 @@
 #         print(i)
 #     i += 1
 
-n1 = int(input("Input Number: "))
-n2 = int(input("Input Number: "))
-high = 0
-if n1 > n2:
-    high = n1
-else:
-    high = n2
-for i in range(1, high):
-    if n1 % i == 0 and n2 % i ==0:
-        print(i)
+# n1 = int(input("Input Number: "))
+# n2 = int(input("Input Number: "))
+# high = 0
+# if n1 > n2:
+#     high = n1
+# else:
+#     high = n2
+# for i in range(1, high + 1):
+#     if n1 % i == 0 and n2 % i == 0:
+#         print(i)
+
