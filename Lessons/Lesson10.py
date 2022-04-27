edfe657a8edf8de68a14cfc73ba8a1d63c188d06
MAMN0 <mamno653@gmail.com>
2022-04-27 14:36:08 +0400

    Lesson10

--- Lessons/Lesson10.py
+++ Lessons/Lesson10.py
@@ -6,3 +6,13 @@ for i in range(1, 10):
     else:
         print("*     *")
 
+print()
+
+i = 1
+while i < 11:
+    if i < 6:
+        asterisk_long = i
+    else:
+        asterisk_long -= 1
+    print("*" * asterisk_long)
+    i += 1
