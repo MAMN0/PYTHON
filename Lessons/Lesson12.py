46cd06e66cbc041df91c872f75d1fcd5cd4a184c
MAMN0 <mamno653@gmail.com>
2022-05-06 16:15:38 +0400

    Lesson12

--- Lessons/Lesson12.py
+++ Lessons/Lesson12.py
@@ -1,19 +1,26 @@
-def count(a, text):
+def symbol_count(a, text):
     count = 0
     for i in range(len(text)):
         if a in text[i]:
             count += 1
     return count
 
+def place_char(a, text):
+    symbol_place = ""
+    for i in range(len(text)):
+        if text[i] == a:
+            symbol_place += str(i) + " "
+    return symbol_place
 symbol = ""
-symbol_count = 0
+symbol_count_v = 0
 
 
 text = input("> ")
 
 for i in range(len(text)):
-    if count(text[i], text) > symbol_count:
+    if symbol_count(text[i], text) > symbol_count_v:
         symbol = text[i]
-        symbol_count = count(text[i], text)
+        symbol_count_v = symbol_count(text[i], text)
 print(symbol)
-print(symbol_count)
+print(place_char(symbol, text))
+print(symbol_count_v)
