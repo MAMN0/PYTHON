adec0ca63aab9936245c97eeae5e3d6fc96bd644
MAMN0 <mamno653@gmail.com>
2022-05-13 16:05:16 +0400

    Lesson15

--- Lessons/Lesson15.py
+++ Lessons/Lesson15.py
@@ -1,11 +1,13 @@
-file_ojbect = open('text.txt', 'r')
-file_ojbect2 = open('test2.txt', "r")
-file_ojbect3 = open('text3.txt', "w")
+file_obj = open('text.txt', 'r')
+# file_obj2 = open('test2.txt', "r")
+# file_obj3 = open('text3.txt', "w")
 
-i = file_ojbect.read()
-n = file_ojbect2.read()
-file_ojbect3.write(n + i)
+i = file_obj.read()
+# n = file_ojbect2.read()
+# file_ojbect3.write(n + i)
+print(i.upper())
 
-file_ojbect3.close()
-file_ojbect2.close()
-file_ojbect.close()
+
+file_obj.close()
+# file_obj2.close()
+# file_obj3.close()
