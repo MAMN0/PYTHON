3fa386bb68a553521f6a9fbf86c796766e840a89
MAMN0 <mamno653@gmail.com>
2022-05-13 15:15:16 +0400

    Homework14

---
+++ Homeworks/Homework14.py
@@ -0,0 +1,17 @@
+#1. დაწერეთ პროგრამა, რომლის მეშვეობით შექმნით ფაილს იმავე დირექტორიაში (საქაღალდეში), ჩაწერეთ მასში ნებისმიერი ტექსტი და დახურეთ ფაილი.
+file_object = open('homeworkfile.txt', 'r')
+print(file_object.read())
+file_object.close()
+# 2. დაწერეთ პროგრამა, რომლის მეშვეობით გახსნით ფაილს, წაიკითხავთ კონტენტს და დაბეჭდავთ ეკრანზე. დაითვალეთ სიმბოლოების რაოდენობა ფაილში.
+file_object = open('homeworkfile.txt', 'r')
+print(len(file_object.read()))
+file_object.close()
+# #3. დაწერეთ პროგრამა, რომლის მეშვეობით გახსნით უკვე არსებულ ფაილს და ბოლოში დაამატეთ თქვენთვის სასურველი ტექსტი.
+file_object = open('homeworkfile.txt', 'a')
+file_object.write('\nhello world')
+file_object.close()
+#4. დაწერეთ პროგრამა რომელიც წაიკითხავს ინფორმაციას ერთი ფაილიდან და ჩააკოპირებს მეორე (ახალ) ფაილში.
+file_object = open('homeworkfile.txt', 'r')
+copytext = file_object.read()
+file_object2 = open('testfile.txt', 'a')
+file_object2.write(copytext)
